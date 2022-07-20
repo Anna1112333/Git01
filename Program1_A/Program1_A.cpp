c8610fb63468d28ea91247cfcd38ba52e9a6d937
Anna1112333 <108662066+Anna1112333@users.noreply.github.com>
2022-07-20 21:14:28 +0500

    Первый проект

---
+++ Program1_A/Program1_A.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int number1, number2;
+    ifstream m("in.txt");
+    if(!m.is_open())
+    {
+        cout << "Файл не открывается"; return 0;
+    }
+    m >> number1;
+    number1;
+    int* ms1 = static_cast<int*>(malloc(number1 * sizeof(int)));
+    for(int i=0; i<number1; ++i)
+    {
+        m >> ms1[i];
+        cout << ms1[i];
+    }
+    cout << "\n";
+    m >> number2;
+    number2;
+    int* ms2 = static_cast<int*>(malloc(number2 * sizeof(int)));
+    for (int i = 0; i < number2; ++i)
+    {
+        m >> ms2[i];
+        cout << ms2[i];
+    }
+    cout << "\n";
+    ofstream j("out.txt");
+        j << number2<<endl<<ms2[number2-1]<<" ";
+        for(int i=0; i<number2-1; i++)
+        {
+            j << ms2[i] << " ";
+        }
+        
+        j <<endl<< number1 << endl << ms1[number1 - 1]<<" ";
+        for (int i = 0; i < number1 - 1; ++i)
+        {
+            j << ms1[i] << " ";
+        }
+       
+    free(ms1);
+    free(ms2);
+    j.close();
+}
